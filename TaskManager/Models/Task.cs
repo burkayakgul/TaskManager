e9087de65a08e7ccab05ab353943315ed7ec9dbe
Burkay Akgül <burkayakgul@hotmail.com>
2022-05-07 04:10:39 +0300

    Model sınıfının oluşturulması ve nuget package üzerinden bootstrap dosyalarının indirme işlemi.

---
+++ TaskManager/Models/Task.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace TaskManager.Models
+{   
+
+    public enum TaskStatus
+    {
+        notStarted,
+        started,
+        finished
+    }
+
+    public enum Priority
+    {
+        High,
+        Medium,
+        Low
+    }
+    public class Task
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string header { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string content { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime taskDate { get; set; }
+        public TaskStatus status { get; set; }
+        public DateTime adjourn { get; set; }
+
+        public Priority priority { get; set; }
+
+    }
+}
